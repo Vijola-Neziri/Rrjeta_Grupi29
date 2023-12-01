e231d0d340952b62fd00e570d16dabcf9b8bbd07
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2023-12-01 14:30:43 +0100

    read privilege

--- src/UDPClient1.java
+++ src/UDPClient1.java
@@ -1,5 +1,23 @@
-public class UDPClient1 {
-    private static final int SERVER_PORT = 9876;
-    private static final String SERVER_IP = "127.0.0.1";
+import java.net.*;
+import java.util.Scanner;
 
+    public class UDPClient1 {
+        private static final int SERVER_PORT = 9876;
+        private static final String SERVER_IP = "192.168.1.9"; //visari
+
+        public static void main(String[] args) {
+            try {
+                DatagramSocket clientSocket = new DatagramSocket();
+
+                InetAddress serverAddress = InetAddress.getByName(SERVER_IP);
+
+                String requestType = "READ";
+
+                System.out.println("You can only choose the READ option.");
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter the file name:");
+                String fileName = scanner.nextLine();
+
+                String message = requestType.toUpperCase() + " " + fileName.toUpperCase();
 }

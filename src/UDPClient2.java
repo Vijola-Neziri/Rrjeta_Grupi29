bca6566a2a1d8c78e23cdd6cece8f1d80fa14191
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2023-12-02 16:18:02 +0100

    server response

--- src/UDPClient2.java
+++ src/UDPClient2.java
@@ -21,6 +21,22 @@ public class UDPClient2 {
             String fileName = scanner.nextLine();
 
             String message = requestType.toUpperCase() + " " + fileName.toUpperCase();
+
+            byte[] sendData = message.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, SERVER_PORT);
+
+            clientSocket.send(sendPacket);
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+
+            String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Server response: " + serverResponse);
+
+            clientSocket.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }

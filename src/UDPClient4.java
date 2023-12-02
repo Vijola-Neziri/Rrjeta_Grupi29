7d80fe71120fcd38267daede2957a3b9bc16ae55
Veronakabashi1 <116003282+Veronakabashi1@users.noreply.github.com>
2023-12-02 16:53:43 +0100

    client4..

--- src/UDPClient4.java
+++ src/UDPClient4.java
@@ -1,3 +1,4 @@
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
@@ -20,6 +21,22 @@ public class UDPClient4 {
             String fileName = scanner.nextLine();
 
             String message = requestType.toUpperCase() + " " + fileName.toUpperCase();
+            byte[] sendData = message.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, SERVER_PORT);
+
+            clientSocket.send(sendPacket);
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+
+
+            String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Server response: " + serverResponse);
+
+            clientSocket.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }

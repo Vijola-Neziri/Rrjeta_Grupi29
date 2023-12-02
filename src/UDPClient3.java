757a575547be50b7e08c4b8f1949fe81542d1e1e
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2023-12-02 16:32:36 +0100

    read permission for client3

--- src/UDPClient3.java
+++ src/UDPClient3.java
@@ -16,6 +16,30 @@ public class UDPClient3 {
             String requestType = "READ";
 
             System.out.println("You can only choose the READ option.");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the file name:");
+            String fileName = scanner.nextLine();
+
+            String message = requestType.toUpperCase() + " " + fileName.toUpperCase();
+
+            byte[] sendData = message.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, SERVER_PORT);
+
+            clientSocket.send(sendPacket);
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+
+            String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Server response: " + serverResponse);
+
+            clientSocket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
+
